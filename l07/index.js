3bec1bcc7aabe56d1d121b9b88a4b13bab2e832c
root <antonio_josemr@hotmail.com>
2020-03-24 18:55:17 +0000

    lab07

---
+++ l07/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 80;
+
+var contacts = [
+	{ 
+		name: "peter",
+		phone: 123456	
+	},
+	{ 
+		name: "pablo",
+		phone: 789456	
+	}
+];
+
+const BASE_API_URL = "/api/v1";
+
+// GET CONTACTS
+
+app.get(BASE_API_URL+"/contacts", (req,res) =>{
+	res.send(JSON.stringify(contacts,null,2));
+	console.log("Data sent:"+JSON.stringify(contacts,null,2));
+});
+
+
+// POST CONTACTS
+
+app.post(BASE_API_URL+"/contacts",(req,res) =>{
+	
+	var newContact = req.body;
+	
+	if((newContact == "") || (newContact.name == null)){
+		res.sendStatus(400,"BAD REQUEST");
+	} else {
+		contacts.push(newContact); 	
+		res.sendStatus(201,"CREATED");
+	}
+});
+
+// DELETE CONTACTS
+
+// GET CONTACT/XXX
+
+app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	
+	var name = req.params.name;
+	
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name == name);
+	});
+	
+	
+	if(filteredContacts.length >= 1){
+		res.send(filteredContacts[0]);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND");
+	}
+});
+
+// PUT CONTACT/XXX
+
+// DELETE CONTACT/XXX
+
+app.delete(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	
+	var name = req.params.name;
+	
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name != name);
+	});
+	
+	
+	if(filteredContacts.length < contacts.length){
+		contacts = filteredContacts;
+		res.sendStatus(200);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND");
+	}
+	
+	
+});
+
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
